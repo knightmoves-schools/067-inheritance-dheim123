5f6e9eaf65032e986d59d6d9f97c54fd53706824
dheim123 <dheim@midland.k12.ia.us>
2025-12-02 12:22:17 -0600

    Update index.js

--- index.js
+++ index.js
@@ -10,31 +10,32 @@ class Agable {
 }
 
 
-class Company extends Agable{
+class Company extends Agable {
     constructor(name, taxId, yearEstablished, taxRate){
+        super(yearEstablished);
         this.name = name;
         this.id = taxId;
-        super(yearEstablished);
         this.taxRate = taxRate;
     }
 }
 
-class Person extends Agable{
+class Person extends Agable {
     constructor(name, ssn, birthYear, taxRate){
+        super(birthYear);
         this.name = name;
         this.id = ssn;
-        super(birthYear);
         this.taxRate = taxRate;
     }
 }
 
-class Car extends Agable{
+class Car extends Agable {
     constructor(model, vin, year){
+        super(year);
         this.name = model;
         this.id = vin;
-        super(year);
     }
 }
+
 
 
 
